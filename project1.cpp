d8864d81deb317e8a8aa4c3d2156b6fbf03ec092
Siona <sisimustafa31@gmail.com>
2025-01-20 18:33:35 +0100

    krijimi i struktures studenti dhe definimi i funksioneve bazike

--- project1.cpp
+++ project1.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+struct Studenti{
+
+  string emri;
+  string mbiemri;
+  string ID;
+  int notat[6];
+
+
+  void lexoStudentin();
+  void shtypStudentin();
+  double notaMesatare();
+  bool fitonBurse();
+
+
+};
+
 int main(){
-    cout<<'Hello world'<<endl;
+    cout<<"Hello world"<<endl;
     return 0;
 }
