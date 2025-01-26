f8dd6e7e66f915d9b317d865bf448193da33dc00
Siona <sisimustafa31@gmail.com>
2025-01-26 18:45:46 +0100

    rregullim

--- project1.cpp
+++ project1.cpp
@@ -44,53 +44,6 @@ void Studenti::lexoStudentin() {
 
 
 
-int main() {
-    
-    cout << "Hello world" << endl;
-    return 0;
-}
-
-
-
-#include <iostream>
-using namespace std;
-
-enum lendet { MATEMATIKE, ELEKTRO, PROGRAMIM, FIZIKE, SH_KOMUNIKUESE, VEGLA_SOFTUERIKE };
-
-struct Studenti {
-    string emri;
-    string mbiemri;
-    string ID;
-    int notat[6];
-    lendet studentii;
-
-    void lexoStudentin();
-    void shtypStudentin();
-    double notaMesatare();
-    bool fitonBurse();
-};
-
-void Studenti::lexoStudentin() {
-    cout << "Sheno emrin e studentit:" << endl;
-    cin >> emri;
-    cout << "Sheno mbiemrin e studentit:" << endl;
-    cin >> mbiemri;
-    cout << "Sheno ID e studentit:" << endl;
-    cin >> ID;
-
-    for (int i = 0; i < 6; i++) {
-        cout << "Shkruaj noten ne lenden "
-            << (i == MATEMATIKE ? "Matematike" :
-                i == ELEKTRO ? "Elektro" :
-                i == PROGRAMIM ? "Programim" :
-                i == FIZIKE ? "Fizike" :
-                i == SH_KOMUNIKUESE ? "Shkathesi Komunikuese" :
-                "Vegla Softuerike")
-            << ": ";
-        cin >> notat[i];
-    }
-}
-
 double Studenti::notaMesatare() {
     double shuma = 0;
     for (int i = 0; i < 6; i++) {
@@ -105,3 +58,8 @@ int main() {
     cout << "Mesatarja e notave te studentit eshte: " << student.notaMesatare() << endl;
     return 0;
 }
+
+
+
+
+
