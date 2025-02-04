5863b72f336a8e56c2626a5a92aea693d612511e
Altinmulaj <>
2025-02-04 23:00:51 +0100

    New and Finished.
    Shtimi i funksioneve dhe persosja e programit.

--- project1.cpp
+++ project1.cpp
@@ -15,19 +15,16 @@ struct Studenti {
     void shtypStudentin();
     double notaMesatare();
     bool fitonBurse();
-
-
-
 };
 
 
 
 void Studenti::lexoStudentin() {
-    cout << "Sheno emrin e studentit:" << endl;
+    cout << "Sheno emrin e studentit..... ";
     cin >> emri;
-    cout << "Sheno mbiemrin e studentit:" << endl;
+    cout << "Sheno mbiemrin e studentit.. ";
     cin >> mbiemri;
-    cout << "Sheno ID e studentit:" << endl;
+    cout << "Sheno ID e studentit........ ";
     cin >> ID;
 
     for (int i = 0; i < 6; i++) {
@@ -43,14 +40,42 @@ void Studenti::lexoStudentin() {
     }
 }
 
-
+void Studenti::shtypStudentin(){
+    cout << endl << " ===== Te dhenat e Studentit =====" << endl;
+    cout << "Emri i Studentit eshte..... " << emri << endl;
+    cout << "Mbiemri i Studentit eshte.. " << mbiemri << endl;
+    cout << "ID e Studentit eshte....... " << ID << endl<<endl;
+    cout << "Notat e Studentit: " << endl;
+    for (int i = 0; i < 6; i++) {
+            cout <<""<< (i == MATEMATIKE ? "Matematike............. " :
+                i == ELEKTRO ?             "Elektro................ " :
+                i == PROGRAMIM ?           "Programim.............. " :
+                i == FIZIKE ?              "Fizike................. " :
+                i == SH_KOMUNIKUESE ?      "Shkathesi Komunikuese.. " :
+                                           "Vegla Softuerike....... ") 
+            << notat[i] << endl;
+    }
+    cout << endl << "Mesatarja e notave te studentit eshte: " << Studenti::notaMesatare() << endl;
+    cout << "Studenti Fiton burse nese 1 = " << Studenti::fitonBurse() << endl;
+}
 
 double Studenti::notaMesatare() {
     double shuma = 0;
+    double avg = 0;
     for (int i = 0; i < 6; i++) {
         shuma += notat[i];
     }
-    return shuma / 6.0; 
+    avg = shuma / 6.0;  // Pjesetimi me 6 do t'jap mesataren
+    return avg; 
+}
+
+bool Studenti::fitonBurse(){
+    if (Studenti::notaMesatare() >= 8.0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 int main() {
@@ -58,12 +83,6 @@ int main() {
     Studenti student;
 
     student.lexoStudentin();
-    
-    cout << "Mesatarja e notave te studentit eshte: " << student.notaMesatare() << endl;
+    student.shtypStudentin();
     return 0;
 }
-
-
-
-
-
